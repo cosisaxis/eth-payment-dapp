91697c20f76c7de453a859e0d333208bdb92f234
cosisaxis <kishanelewis2@gmail.com>
2022-03-22 11:23:45 -0500

    UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="m-4">
+      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white ">
+        <main className="mt-4 p-4">
+          <h1 className="text-xl font-semibold text-center">
+            Send ETH
+          </h1>
+          <div>
+            <div className="my-3">
+              <input 
+              type='text'
+              name='addr'
+              className='input input-bordered block w-auto focus:ring focus:outline-none mx-auto'
+              placeholder='Address'
+              />
+            </div>
+            <div className="my-3">
+              <input 
+              type='text'
+              name='addr'
+              className='input input-bordered block w-auto focus:ring focus:outline-none mx-auto'
+              placeholder='Amount'
+              />
+            </div>
+          </div>
+        </main>
+        <footer className="p-4">
+          <button 
+          type="submit"
+          className="btn bg-blue-500 submit-button focus:ring focus:outline-none w-full mx-auto block"
+          >
+          
+            Send transfer
+          </button>
+        </footer>
+      </div>
+    </form>
   );
 }
 
