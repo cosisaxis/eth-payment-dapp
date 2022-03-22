12301e5266330b812c44412646bb8331e8c3b4bb
cosisaxis <kishanelewis2@gmail.com>
2022-03-22 14:22:49 -0500

    final-update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,94 @@
 import { useState } from "react";
 import { ethers } from "ethers";
+import ErrorMsg from "./ErrorMsg";
+import TransactionsList from "./TransactionsList";
+import './index.css'
 
 
-function App() {
+const payment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No wallet was found, please install metamask");
+
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether)
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
+  }
+};
+
+export default function App() {
+  const [error, setError] = useState();
+  const [txs, setTxs] = useState([]);
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError();
+    await payment({
+      setError,
+      setTxs,
+      ether: data.get("ether"),
+      addr: data.get("addr")
+    });
+  };
+
   return (
-    <form className="m-4">
-      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white ">
+    <form className="m-4" onSubmit={submit}>
+      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-black">
         <main className="mt-4 p-4">
-          <h1 className="text-xl font-semibold text-center">
-            Send ETH
+          <h1 className="text-xl font-semibold text-center text-red-500">
+            Send ETH payment
           </h1>
-          <div>
+          <div className="">
             <div className="my-3">
-              <input 
-              type='text'
-              name='addr'
-              className='input input-bordered block w-auto focus:ring focus:outline-none mx-auto'
-              placeholder='Address'
+              <input
+                type="text"
+                name="addr"
+                className="input input-bordered block w-auto mx-auto focus:ring focus:outline-none"
+                placeholder="Recipient Address"
               />
             </div>
             <div className="my-3">
-              <input 
-              type='text'
-              name='addr'
-              className='input input-bordered block w-auto focus:ring focus:outline-none mx-auto'
-              placeholder='Amount'
+              <input
+                name="ether"
+                type="text"
+                className="input input-bordered block w-auto  mx-auto focus:ring focus:outline-none"
+                placeholder="Amount in ETH"
               />
             </div>
           </div>
         </main>
         <footer className="p-4">
-          <button 
-          type="submit"
-          className="btn bg-blue-500 submit-button focus:ring focus:outline-none w-full mx-auto block"
+          <button
+            type="submit"
+            className="btn btn-blue submit-button focus:ring focus:outline-none w-full"
           >
-          
-            Send transfer
+            Pay now
           </button>
+          <ErrorMsg message={error} />
+          <TransactionsList txs={txs} />
+
+          <div>
+            <h2 className=" text-center mt-2 text-white">
+              made by <a href='https://twitter.com/cartieraxis' className="text-red-500">Axis</a>
+            </h2>
+          </div>
         </footer>
       </div>
     </form>
+
+    
   );
+ 
 }
 
-export default App;
--- src/ErrorMsg.js
+++ src/ErrorMsg.js
@@ -2,21 +2,8 @@ export default function ErrorMsg({ message}){
     if (!message) return null;
 
     return(
+    <div>
         <div>
-            <div>
-            <svg
-             xmlns="http://www.w3.org/2000/svg"
-             fill="none"
-             viewBox="0 0 24 24"
-             className="w-6 h-6 mx-2 stroke-current"
-            >   
-             <path
-               stroke-linecap="round"
-               stroke-linejoin="round"
-               stroke-width="2"
-               d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-          ></path>
-        </svg>
           <label>{message}</label>
       </div>
     </div>
